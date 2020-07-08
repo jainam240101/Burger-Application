98a49580979d5504e493e100bd8df457c9be72c5
Jainam Mehta <jainamm47@gmail.com>
2020-07-08 23:57:25 +0530

    typescript-boliperplate

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Typescript React by Jainam</h1>
+    </div>
+  );
+};
+
+export default App;
